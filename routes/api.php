1815e98686667cd96f8f0a0dcac118469864dd30
MONTEIL <monteilpauline65@gmail.com>
2019-07-10 23:55:33 +0200

    .env fix + comments

--- routes/api.php
+++ routes/api.php
@@ -14,11 +14,11 @@ use PhpParser\Node\Stmt\ElseIf_;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+//Route qui redirige vers toute les randonnées
 Route::get('/hike', function (Request $request) {
     return Hike::get();
 });
-
+//Route qui redirige soit vers une randonnée soit vers la dernière 
 Route::get('/hike/{n}', function ($n) {
     if ($n === "last") return Hike::get()->sortByDesc('created_at')->first();
     if (intval($n)) return Hike::get()->where('id', intval($n));
